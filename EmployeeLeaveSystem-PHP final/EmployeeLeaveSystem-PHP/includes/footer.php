90f074ce6dbbbe7f848497901958694dde62fc7d
SNEHA MADDIPATI <108052508+SnehaMaddipati@users.noreply.github.com>
2023-08-21 23:15:41 +0530

    Add files via upload

---
+++ EmployeeLeaveSystem-PHP final/EmployeeLeaveSystem-PHP/includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y"); ?> | Employee Leave Request System | Developed By <a
+                                href="https://jssateb.ac.in/index">Sneha And Sanjana</a></p>
+        </div>
+</footer>
